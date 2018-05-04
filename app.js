93ed9129af37a6a9019972f95428c560fd468b79
Billy Li <bl@medipass.io>
2018-05-04 17:20:52 +1000

    Removed weather API

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const APIKEY = process.env.WU_ACCESS;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -13,7 +12,7 @@ app.get('/', (req, res) => {
     return res.send('Running');
 });
 
-app.post('/generateCharacter', async (req, res) => {
+app.post('/generate', async (req, res) => {
     const raceIndex = Math.floor((Math.random() * 19) + 1);
     const genderIndex = Math.round(Math.random());
     const alignmentIndex = Math.floor((Math.random() * 2) + 1);
@@ -39,38 +38,6 @@ app.post('/generateCharacter', async (req, res) => {
                 + "Gender: " + data.description.gender + "\n"
                 + "Race: " + data.description.race + "\n"
                 + "Occupation: " + data.description.occupation
-            }
-        ]
-    };
-    return res.send(body);
-});
-
-//app.post is triggered when a POST request is sent to the URL ‘/post’
-app.post('/post', async (req, res)=> {
-    //take a message from Slack slash command
-    const query = req.body.text;
-
-    const URL = `http://api.wunderground.com/api/${APIKEY}/conditions/q/${query}.json`;
-
-    const response = await axios({
-        method: 'get',
-        url: URL,
-        response: 'json'
-    });
-
-    const temperature = response.data.current_observation.temperature_string;
-    const weatherCondition = response.data.current_observation.weather;
-    const icon_url = response.data.current_observation.icon_url;
-    const location = response.data.current_observation.display_location.full;
-
-    const body = {
-        response_type: "in_channel",
-        "attachments": [
-            {
-                "text": "Location: " + location + "\n"
-                + "Temperature: " + temperature + "\n"
-                + "Condition: " + weatherCondition,
-                "image_url": icon_url,
             }
         ]
     };
