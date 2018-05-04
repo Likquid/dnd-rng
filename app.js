5b0e765dcd94292c22468b925ca560721072da04
Billy Li <bl@medipass.io>
2018-05-04 18:01:52 +1000

    Parsing other character attributes

--- app.js
+++ app.js
@@ -14,7 +14,35 @@ const characterContext = (data) =>
 Age: ${data.description.age}
 Gender: ${data.description.gender}
 Race: ${data.description.race}
-Occupation: ${data.description.occupation}`;
+Occupation: ${data.description.occupation}
+
+Ability Scores:
+Str: ${data.abilities.str}
+Dex: ${data.abilities.dex}
+Con: ${data.abilities.con}
+Int: ${data.abilities.int}
+Wis: ${data.abilities.wis}
+Cha: ${data.abilities.cha}
+
+Alignment:
+Chaotic: ${data.alignment.chaotic}
+Neutral: ${data.alignment.ethicalneutral}
+Evil: ${data.alignment.evil}
+Good: ${data.alignment.good}
+Lawful: ${data.alignment.Lawful}
+
+Relationships:
+Sexual Orientation: ${data.relationship.orientation}
+Status: ${data.relationship.status}
+
+Traits:
+${data.ptraits.traits1}
+${data.ptraits.traits2}
+${data.pquirks.description}
+${data.religion.description}
+
+Plot Hook: ${data.hook.description}
+`;
 
 app.get('/', (req, res) => {
     return res.send('Running');
